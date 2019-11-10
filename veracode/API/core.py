b0a76c27cea4fdae54c6bb6b7dcd2e11a673fa1f
chuck <chuckorde@gmail.com>
2019-11-09 19:44:43 -0500

    Adding CI config

--- veracode/API/core.py
+++ veracode/API/core.py
@@ -28,12 +28,14 @@ class REST(object):
             conf.read(os.path.expanduser('~/.veracode/credentials'))
             self.__api_id = conf.get('DEFAULT', 'VERACODE_API_ID')
             self.__api_secret = conf.get('DEFAULT', 'VERACODE_API_SECRET')
-
         except FileNotFoundError as e:
-            raise VeracodeConfigError ((
-                'You must configure analysiscenter credentials before using'
-                ' the veracode API module.\n\n'
-                'To configure run: `python -m veracode.configure`'))
+            try:
+                conf = configparser.ConfigParser()
+                conf.read(os.path.expanduser('~/.veracode/credentials'))
+                self.__api_id = conf.get('DEFAULT', 'VERACODE_API_ID')
+                self.__api_secret = conf.get('DEFAULT', 'VERACODE_API_SECRET')
+            except FileNotFoundError as e:
+                raise VeracodeConfigError ('No credentials file found')
 
         self.__api_server = server or 'https://analysiscenter.veracode.com/api/'
         self.__end_point = end_point
@@ -46,7 +48,7 @@ class REST(object):
 
     def __veracode_hmac(self, host, url, method):
         signing_data = 'id={api_id}&host={host}&url={url}&method={method}'.format(
-            api_id=self.__api_id.lower(),                                            
+            api_id=self.__api_id.lower(),
             host=host.lower(),
             url=url, method=method.upper())
 
@@ -54,7 +56,7 @@ class REST(object):
         nonce = os.urandom(16).hex()
 
         key_nonce = hmac.new(
-            codecs.decode(self.__api_secret, 'hex_codec'), 
+            codecs.decode(self.__api_secret, 'hex_codec'),
             codecs.decode(nonce, 'hex_codec'), sha256).digest()
 
         key_date = hmac.new(key_nonce, str(timestamp).encode(), sha256).digest()
@@ -64,13 +66,13 @@ class REST(object):
         return '{auth} id={id},ts={ts},nonce={nonce},sig={sig}'.format(
             auth='VERACODE-HMAC-SHA-256',
             id=self.__api_id,
-            ts=timestamp, 
-            nonce=nonce, 
+            ts=timestamp,
+            nonce=nonce,
             sig=signature)
 
     def __prepared_request(self, method, query, files=None):
         logger.debug('{}, {}, STARTED'.format(self.__end_point, query))
-        
+
         session = requests.Session()
         session.mount(self.__server, HTTPAdapter(max_retries=3))
         request = requests.Request(method, self.__server, params=query)
@@ -78,17 +80,17 @@ class REST(object):
         prepared_request.headers['Authorization'] = self.__veracode_hmac(
             urlparse(self.__server).hostname, prepared_request.path_url, method)
         res = session.send(prepared_request)
-        
+
         logger.debug('{}, {}, COMPLETED'.format(self.__end_point, query))
         logger.info('request URL: {}'.format(res.request.path_url))
-        
+
         return res
 
 
     def GET(self, query=None, format='text'):
         res = self.__prepared_request('GET', query)
         return self.response(res.status_code, getattr(res, format), res)
-        
+
     def POST(self, query=None, file=None):
         res = self.__prepared_request('POST', query, file)
         return self.response(res.status_code, res.text, res)
