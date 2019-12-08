add1fe07458b2d67adce315b02b3e0a7b3e4610b
chuck <chuckorde@gmail.com>
2019-12-08 23:41:51 +0000

    Version 0.1.8 [skip ci]

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
         name = 'veracode-python',
         packages = ['veracode-python'],
-        version = '0.1.7',
+        version = '0.1.8',
         license = 'MIT',
         description = 'Python wrapper for the Veracode APIs',
         author = 'Chuck Orde',
         author_email = 'chuckorde@gmail.com',
         url = 'https://github.com/chuckorde/veracode-python',
-        download_url = 'https://github.com/chuckorde/veracode-python/archive/v0.1.7.tar.gz',
+        download_url = 'https://github.com/chuckorde/veracode-python/archive/v0.1.8.tar.gz',
         keywords = ['Veracode', 'Security'],
         install_requires=[
             'lxml',
