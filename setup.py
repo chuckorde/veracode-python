9f4b1d0d2b7f3068628df9c0b206e6258a7ad38c
chuck <chuckorde@gmail.com>
2019-12-09 21:52:28 +0000

    Version 0.1.52 [skip ci]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 setuptools.setup(
         name = 'veracode-python',
         packages = setuptools.find_packages(),
-        version = '0.1.51',
+        version = '0.1.52',
         license = 'MIT',
         description = 'Python wrapper for the Veracode APIs',
         long_description=long_description,
@@ -14,7 +14,7 @@ setuptools.setup(
         author = 'Chuck Orde',
         author_email = 'chuckorde@gmail.com',
         url = 'https://github.com/chuckorde/veracode-python',
-        download_url = 'https://github.com/chuckorde/veracode-python/archive/v0.1.51.tar.gz',
+        download_url = 'https://github.com/chuckorde/veracode-python/archive/v0.1.52.tar.gz',
         keywords = ['Veracode', 'Security'],
         install_requires=[
             'lxml',
--- veracode/__init__.py
+++ veracode/__init__.py
@@ -1,2 +1,2 @@
 from .application import Application
-__version__ = '0.1.51'
+__version__ = '0.1.52'
