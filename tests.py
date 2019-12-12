d144773432a96386a4ea307c376043e314a3f128
Chuck Orde <chuckorde@gmail.com>
2019-12-12 16:13:16 -0500

    run tests... sanity check

--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 if __name__ == '__main__':
     import doctest
-    from veracode import application, sandbox
+    from veracode import application, sandbox, build
     from veracode.exceptions import *
 
     try:
         doctest.testmod(application, raise_on_error=True)
         doctest.testmod(sandbox, raise_on_error=True)
+        doctest.testmod(build, raise_on_error=True)
     except:
         pass
 
