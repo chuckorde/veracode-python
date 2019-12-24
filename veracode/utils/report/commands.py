c06c16ac1b734f426fc5afa301b3fdd1e2b154f9
Chuck Orde <chuckorde@gmail.com>
2019-12-24 18:07:32 -0500

    more cli updates

--- veracode/utils/report/commands.py
+++ veracode/utils/report/commands.py
@@ -1,12 +1,21 @@
 import click
+from veracode.application import Application
+from veracode.utils.report import display
 
 @click.group()
 def report():
     pass
 
 @report.command()
-def list():
-    click.echo('list')
+@click.option('--app', '-a', required=True,
+        help='Name of the application.')
+@click.option('--sandbox', '-s',
+        help='Name of the application sandbox.')
+@click.option('--build', '-b',
+        help='Name of the application build.')
+def summary(app, sandbox=None, build=None):
+    app = Application(name=app, sandbox=sandbox, build=build)
+    print(app.build.report)
 
 @report.command()
 def create():
